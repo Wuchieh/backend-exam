3bd055f4eadddad049280e236998d4fcc064efaa
shun <shunwuse@gmail.com>
2025-12-18 23:59:35 +0800

    feat: implement swap

--- swap/main.go
+++ swap/main.go
@@ -2,9 +2,50 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-func swap[T any](a, b T) {}
+func swap[T any](a, b T) {
+	valueA := reflect.ValueOf(a)
+	valueB := reflect.ValueOf(b)
+
+	// Only pointers can be swapped.
+	if valueA.Kind() != reflect.Pointer {
+		panic("first argument must be a pointer")
+	}
+	if valueB.Kind() != reflect.Pointer {
+		panic("second argument must be a pointer")
+	}
+
+	// Check nil pointer.
+	if valueA.IsNil() {
+		panic("first argument is a nil pointer")
+	}
+	if valueB.IsNil() {
+		panic("second argument is a nil pointer")
+	}
+
+	elemA := valueA.Elem()
+	elemB := valueB.Elem()
+
+	// Check both are same type.
+	if elemA.Type() != elemB.Type() {
+		panic("arguments are different types")
+	}
+
+	// Check settable.
+	if !elemA.CanSet() {
+		panic("first argument is not settable")
+	}
+	if !elemB.CanSet() {
+		panic("second argument is not settable")
+	}
+
+	// Swap.
+	temp := reflect.ValueOf(elemA.Interface())
+	elemA.Set(elemB)
+	elemB.Set(temp)
+}
 
 func main() {
 	a := 10
